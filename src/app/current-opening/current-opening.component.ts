1dd20a46cd00b4eaefbcbad6dfded2633fe21b30
ajay <ajay@akscellenceinfo.com>
2020-11-25 16:59:59 +0530

    Two new job post and update contact us details

--- src/app/current-opening/current-opening.component.ts
+++ src/app/current-opening/current-opening.component.ts
@@ -9,33 +9,33 @@ import { MetaTagAndTitleService } from "../Service and Model/metaTags-Title.serv
 export class CurrentOpeningComponent implements OnInit {
   ajay: number = 1;
 
-  current: any[] = [    
+  current: any[] = [
     {
       id: 1,
-      titel: "Microsoft Power BI Developer - Data Visualization & Analysis",
+      titel: "Oracle Technical/Functional Consultant - Any Module for Bangkok/Gurgaon, Haryana",
       dis: [
         {
-          Job_Location: "Gurgaon, Haryana",
-          Job_Overview: "Microsoft Power BI Developer - Data Visualization & Analysis",
-          Responsibilities_Duties: `<ol>
-                                    <li><span>0 - 2 years experience developing Power BI solutions and reporting</span></li>
-                                    <li><span>0 - 2 years experience in Data Visualization and Analysis</span></li>
-                                    <li><span>0 - 2 years experience in Excel Pivot/vLookup/Reports etc.</span></li>
-                                   </ol>
-                                   <p><span>Experience working with dataset ingestion, data model creation, reports, dashboards, KPIs, Power BI Pro features, workspaces, security, etc.</span></p>`,
-          Qualifications: "Required Qualifications Bachelor's degree or equivalent experience",
-          Preferred_Qualifications: `<ol>
-                                    <li><span>ETL, SSAS and SSIS experience.</span></li>
-                                    <li><span>Strong data modeling experience.</span></li>
-                                    <li><span>Experience in building data marts.</span></li>
-                                    <li><span>Experience working with a variety of stakeholders, from managers to technical resources, to include the translation of business requirements into operational measures and reports.</span></li>
-                                    <li><span>AgileScrum project experience nice to have.</span></li>
-                                    <li><span>High level of demonstrable proficiency with Microsoft Excel, including Power Query, Power Pivot.</span></li>
-                                    <li><span>Connections to external data sources, REST APIs, pivot tables, charts, and slicers.</span></li>
-                                    <li><span>Proven ability to create, publish, and manage content packs.</span></li>
-                                    <li><span>Deep understanding of security as it pertains to data access, scope, and distribution of reports and Dashboards.</span></li>
-                                   </ol>`,
-          Contact_Us: `<p><span>Interested applicants, please share your profile at <a href="mailto:pranay@akscellenceinfo.com">pranay@akscellenceinfo.com</a> or you can reach me at my Mobile# +91-98112-33516, Thanks...</span></p>`,
+          Job_Location: "Bangkok/Gurgaon, Haryana",
+          Job_Overview: "Oracle Technical/Functional Consultant – Any Module",
+          // Responsibilities_Duties: `<ol>
+          //                           <li><span>0 - 2 years experience developing Power BI solutions and reporting</span></li>
+          //                           <li><span>0 - 2 years experience in Data Visualization and Analysis</span></li>
+          //                           <li><span>0 - 2 years experience in Excel Pivot/vLookup/Reports etc.</span></li>
+          //                          </ol>
+          //                          <p><span>Experience working with dataset ingestion, data model creation, reports, dashboards, KPIs, Power BI Pro features, workspaces, security, etc.</span></p>`,
+          // Qualifications: "Required Qualifications Bachelor's degree or equivalent experience",
+          // Preferred_Qualifications: `<ol>
+          //                           <li><span>ETL, SSAS and SSIS experience.</span></li>
+          //                           <li><span>Strong data modeling experience.</span></li>
+          //                           <li><span>Experience in building data marts.</span></li>
+          //                           <li><span>Experience working with a variety of stakeholders, from managers to technical resources, to include the translation of business requirements into operational measures and reports.</span></li>
+          //                           <li><span>AgileScrum project experience nice to have.</span></li>
+          //                           <li><span>High level of demonstrable proficiency with Microsoft Excel, including Power Query, Power Pivot.</span></li>
+          //                           <li><span>Connections to external data sources, REST APIs, pivot tables, charts, and slicers.</span></li>
+          //                           <li><span>Proven ability to create, publish, and manage content packs.</span></li>
+          //                           <li><span>Deep understanding of security as it pertains to data access, scope, and distribution of reports and Dashboards.</span></li>
+          //                          </ol>`,
+          // Contact_Us: `<p><span>Interested applicants, please share your profile at <a href="mailto:pranay@akscellenceinfo.com">pranay@akscellenceinfo.com</a> or you can reach me at my Mobile# +91-98112-33516, Thanks...</span></p>`,
           
         }
       ]
@@ -43,30 +43,92 @@ export class CurrentOpeningComponent implements OnInit {
     {
       id: 2,
       hide: true,
+      titel: "DotNet + React - Dot Net Framework, ASP.NET, C# for Hyderabad",
+      dis: [
+        {
+          Job_Location: "Hyderabad",
+          Job_Overview: "DotNet + React - Dot Net Framework, ASP.NET, C#",
+          // Responsibilities_Duties: `<ol>
+          //                           <li><span>0 - 2 years experience developing Power BI solutions and reporting</span></li>
+          //                           <li><span>0 - 2 years experience in Data Visualization and Analysis</span></li>
+          //                           <li><span>0 - 2 years experience in Excel Pivot/vLookup/Reports etc.</span></li>
+          //                          </ol>
+          //                          <p><span>Experience working with dataset ingestion, data model creation, reports, dashboards, KPIs, Power BI Pro features, workspaces, security, etc.</span></p>`,
+          // Qualifications: "Required Qualifications Bachelor's degree or equivalent experience",
+          // Preferred_Qualifications: `<ol>
+          //                           <li><span>ETL, SSAS and SSIS experience.</span></li>
+          //                           <li><span>Strong data modeling experience.</span></li>
+          //                           <li><span>Experience in building data marts.</span></li>
+          //                           <li><span>Experience working with a variety of stakeholders, from managers to technical resources, to include the translation of business requirements into operational measures and reports.</span></li>
+          //                           <li><span>AgileScrum project experience nice to have.</span></li>
+          //                           <li><span>High level of demonstrable proficiency with Microsoft Excel, including Power Query, Power Pivot.</span></li>
+          //                           <li><span>Connections to external data sources, REST APIs, pivot tables, charts, and slicers.</span></li>
+          //                           <li><span>Proven ability to create, publish, and manage content packs.</span></li>
+          //                           <li><span>Deep understanding of security as it pertains to data access, scope, and distribution of reports and Dashboards.</span></li>
+          //                          </ol>`,
+          // Contact_Us: `<p><span>Interested applicants, please share your profile at <a href="mailto:pranay@akscellenceinfo.com">pranay@akscellenceinfo.com</a> or you can reach me at my Mobile# +91-98112-33516, Thanks...</span></p>`,
+          hide: true
+        }
+      ]
+    },
+    {
+      id: 3,
+      hide: true,
+      titel: "Microsoft Power BI Developer - Data Visualization & Analysis",
+      dis: [
+        {
+          Job_Location: "Gurgaon, Haryana",
+          Job_Overview: "Microsoft Power BI Developer - Data Visualization & Analysis",
+          // Responsibilities_Duties: `<ol>
+          //                           <li><span>0 - 2 years experience developing Power BI solutions and reporting</span></li>
+          //                           <li><span>0 - 2 years experience in Data Visualization and Analysis</span></li>
+          //                           <li><span>0 - 2 years experience in Excel Pivot/vLookup/Reports etc.</span></li>
+          //                          </ol>
+          //                          <p><span>Experience working with dataset ingestion, data model creation, reports, dashboards, KPIs, Power BI Pro features, workspaces, security, etc.</span></p>`,
+          // Qualifications: "Required Qualifications Bachelor's degree or equivalent experience",
+          // Preferred_Qualifications: `<ol>
+          //                           <li><span>ETL, SSAS and SSIS experience.</span></li>
+          //                           <li><span>Strong data modeling experience.</span></li>
+          //                           <li><span>Experience in building data marts.</span></li>
+          //                           <li><span>Experience working with a variety of stakeholders, from managers to technical resources, to include the translation of business requirements into operational measures and reports.</span></li>
+          //                           <li><span>AgileScrum project experience nice to have.</span></li>
+          //                           <li><span>High level of demonstrable proficiency with Microsoft Excel, including Power Query, Power Pivot.</span></li>
+          //                           <li><span>Connections to external data sources, REST APIs, pivot tables, charts, and slicers.</span></li>
+          //                           <li><span>Proven ability to create, publish, and manage content packs.</span></li>
+          //                           <li><span>Deep understanding of security as it pertains to data access, scope, and distribution of reports and Dashboards.</span></li>
+          //                          </ol>`,
+          // Contact_Us: `<p><span>Interested applicants, please share your profile at <a href="mailto:pranay@akscellenceinfo.com">pranay@akscellenceinfo.com</a> or you can reach me at my Mobile# +91-98112-33516, Thanks...</span></p>`,
+          hide: true
+        }
+      ]
+    },
+    {
+      id: 4,
+      hide: true,
       titel: "Azure Cloud Lead with up to 10 years of experience on Data Factory, Lake, Brick and Synapse",
       dis: [
         {
           Job_Location: "Gurgaon, Haryana",
           Job_Overview: "Microsoft Power BI Developer - Data Visualization & Analysis",
-          Responsibilities_Duties: `<ol>
-                                    <li><span>0 - 2 years experience developing Power BI solutions and reporting</span></li>
-                                    <li><span>0 - 2 years experience in Data Visualization and Analysis</span></li>
-                                    <li><span>0 - 2 years experience in Excel Pivot/vLookup/Reports etc.</span></li>
-                                   </ol>
-                                   <p><span>Experience working with dataset ingestion, data model creation, reports, dashboards, KPIs, Power BI Pro features, workspaces, security, etc.</span></p>`,
-          Qualifications: "Required Qualifications Bachelor's degree or equivalent experience",
-          Preferred_Qualifications: `<ol>
-                                    <li><span>ETL, SSAS and SSIS experience.</span></li>
-                                    <li><span>Strong data modeling experience.</span></li>
-                                    <li><span>Experience in building data marts.</span></li>
-                                    <li><span>Experience working with a variety of stakeholders, from managers to technical resources, to include the translation of business requirements into operational measures and reports.</span></li>
-                                    <li><span>AgileScrum project experience nice to have.</span></li>
-                                    <li><span>High level of demonstrable proficiency with Microsoft Excel, including Power Query, Power Pivot.</span></li>
-                                    <li><span>Connections to external data sources, REST APIs, pivot tables, charts, and slicers.</span></li>
-                                    <li><span>Proven ability to create, publish, and manage content packs.</span></li>
-                                    <li><span>Deep understanding of security as it pertains to data access, scope, and distribution of reports and Dashboards.</span></li>
-                                   </ol>`,
-          Contact_Us: `<p><span>Interested applicants, please share your profile at <a href="mailto:pranay@akscellenceinfo.com">pranay@akscellenceinfo.com</a> or you can reach me at my Mobile# +91-98112-33516, Thanks...</span></p>`,
+          // Responsibilities_Duties: `<ol>
+          //                           <li><span>0 - 2 years experience developing Power BI solutions and reporting</span></li>
+          //                           <li><span>0 - 2 years experience in Data Visualization and Analysis</span></li>
+          //                           <li><span>0 - 2 years experience in Excel Pivot/vLookup/Reports etc.</span></li>
+          //                          </ol>
+          //                          <p><span>Experience working with dataset ingestion, data model creation, reports, dashboards, KPIs, Power BI Pro features, workspaces, security, etc.</span></p>`,
+          // Qualifications: "Required Qualifications Bachelor's degree or equivalent experience",
+          // Preferred_Qualifications: `<ol>
+          //                           <li><span>ETL, SSAS and SSIS experience.</span></li>
+          //                           <li><span>Strong data modeling experience.</span></li>
+          //                           <li><span>Experience in building data marts.</span></li>
+          //                           <li><span>Experience working with a variety of stakeholders, from managers to technical resources, to include the translation of business requirements into operational measures and reports.</span></li>
+          //                           <li><span>AgileScrum project experience nice to have.</span></li>
+          //                           <li><span>High level of demonstrable proficiency with Microsoft Excel, including Power Query, Power Pivot.</span></li>
+          //                           <li><span>Connections to external data sources, REST APIs, pivot tables, charts, and slicers.</span></li>
+          //                           <li><span>Proven ability to create, publish, and manage content packs.</span></li>
+          //                           <li><span>Deep understanding of security as it pertains to data access, scope, and distribution of reports and Dashboards.</span></li>
+          //                          </ol>`,
+          // Contact_Us: `<p><span>Interested applicants, please share your profile at <a href="mailto:pranay@akscellenceinfo.com">pranay@akscellenceinfo.com</a> or you can reach me at my Mobile# +91-98112-33516, Thanks...</span></p>`,
           hide: true
         }
       ]
@@ -88,6 +150,9 @@ export class CurrentOpeningComponent implements OnInit {
     }
     if (this.ajay == 3) {
       console.log(this.current[2]);
+    }
+    if (this.ajay == 4) {
+      console.log(this.current[3]);
     }
     // if (id == 1)
     //   this.ajay = this.current[0].dis[0].Location;
